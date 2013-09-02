1743133f221f4a1e50bab21cfa2f52a7f891bd0e
luis Perez <luis.perez.nec@gmail.com>
2013-09-02 06:23:24 -0500

    Programa Suma
    Programa que realiza la suma de dos numeros

---
+++ CIC_IPN/Suma/src/test/Suma.java
@@ -0,0 +1,16 @@
+/**
+ * programa que realiza la suma de dos numeros
+ */
+package test;
+
+public class Suma {
+
+	public static void main(String[] args){
+		int num1 = 11;
+		int num2 = 34;
+		
+		System.out.printf("La suma de los numeros es: %d", num1 + num2);
+		
+	}
+
+}
