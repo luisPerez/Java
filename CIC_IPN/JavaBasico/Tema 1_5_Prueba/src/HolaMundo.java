10e7efecd05c579ac7c2651f49e08ef948331bc0
luis Perez <luis.perez.nec@gmail.com>
2013-09-02 06:07:35 -0500

    primer Programa
    Programa del curso de Java Basico CIC IPN

---
+++ CIC_IPN/JavaBasico/Tema 1_5_Prueba/src/HolaMundo.java
@@ -0,0 +1,12 @@
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// printf imprime una cadena con formato
+		// %n es un salto de linea
+		System.out.printf("hola, Mundo%n");
+	}
+}
